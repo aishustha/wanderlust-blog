a09fba24ac045409ff1d4dafdaeaba8c9f77c82b
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2024-02-04 21:50:11 -0500

    Seo added for metadata

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from './layout'
+import Seo from './seo'
 
 
 const AboutPage = () => {
@@ -16,10 +17,5 @@ const AboutPage = () => {
 }
 
 
-export const Head = () => (
-    <>
-    <title> About Wanderlust</title>
-    <meta name="description" content="Your description"/>
-    </>
-)
+export const Head = () => <Seo title="About Wanderlust"/>
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "./layout"
 import { StaticImage } from "gatsby-plugin-image"
-
+import Seo from "./seo"
 
 //Defining components
 const IndexPage = () => {
@@ -22,7 +22,7 @@ const IndexPage = () => {
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page"/>
 
 export default IndexPage
 
